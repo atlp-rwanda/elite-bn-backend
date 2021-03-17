b2730c563aeb1bfaf9b92d09e488ff8e5c2cb15a
Alexis-Vacilli <alexisvacilli1@gmail.com>
2021-03-17 18:56:48 +0200

    added redirection for verifying the email

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -18,40 +18,43 @@ const util = new Util();
 
 class User {
     static signUp = async (req, res) => {
-        const {
-            email,
-            firstName,
-            password,
-            lastName
-        } = req.body;
-        const newUser = {
-            email,
-            password: await bcrypt.hash(password, 10),
-            firstName,
-            lastName
-        };
-
-        const createdUser = await userServices.createuser(newUser);
-        const createdUserDetails = createdUser.dataValues;
-
-        const token = jwt.sign({
-            id: createdUserDetails.id,
-            email: createdUserDetails.email
-        }, process.env.PRIVATE_KEY, {
-            expiresIn: '7d'
-        });
-        const subject = 'Verify email for Barefoot Nomad';
-
-        sendEmail(emailTemplate(token, createdUserDetails.firstName), subject, email);
-        const message = `Dear ${createdUserDetails.firstName} , A verification email has been sent to you email please go and confirm that email.`;
-        const data = {
-            id: createdUserDetails.id,
-            email: createdUserDetails.email,
-            token,
-        };
-        util.setSuccess(201, message, data);
-        return util.send(res);
-
+        try{
+            const {
+                email,
+                firstName,
+                password,
+                lastName
+            } = req.body;
+            const newUser = {
+                email,
+                password: await bcrypt.hash(password, 10),
+                firstName,
+                lastName
+            };
+    
+            const createdUser = await userServices.createuser(newUser);
+            const createdUserDetails = createdUser.dataValues;
+            const token = jwt.sign({
+                id: createdUserDetails.id,
+                email: createdUserDetails.email
+            }, process.env.PRIVATE_KEY, {
+                expiresIn: '7d'
+            });
+            const subject = 'Verify email for Barefoot Nomad';
+    
+            sendEmail(emailTemplate(token, createdUserDetails.firstName), subject, email);
+            const message = `Dear ${createdUserDetails.firstName} , A verification email has been sent to you email please go and confirm that email.`;
+            const data = {
+                id: createdUserDetails.id,
+                email: createdUserDetails.email,
+                token,
+            };
+            util.setSuccess(201, message, data);
+            return util.send(res);
+        } catch(err) {
+            util.setError(400, error);
+            return util.send(res);
+        }
 
     }
 
@@ -66,9 +69,10 @@ class User {
             }, {
                 email: decodeToken.email
             });
-            const message = 'Account was succesfully verified';
-            util.setSuccess(200, message);
-            return util.send(res);
+            // const message = 'Account was succesfully verified';
+            // util.setSuccess(200, message);
+            // return util.send(res);
+            res.redirect(`${process.env.FRONT_END_URL}/login`);
         } catch (error) {
             util.setError(410, error);
             return util.send(res);
