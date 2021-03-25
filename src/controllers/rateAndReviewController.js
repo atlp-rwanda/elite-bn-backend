65071017fe1e523b669a2ce943fc4e6faace1303
shemaM <azertshema@gmail.com>
2021-03-25 10:40:57 +0200

    ft(multiple-languages):translating responses into multi langueages

--- src/controllers/rateAndReviewController.js
+++ src/controllers/rateAndReviewController.js
@@ -10,10 +10,13 @@ class ReviewController {
       const ratingInformation = req.body;
       ratingInformation.userId = req.userId;
       const rateAcc = await RatingService.addRating(ratingInformation);
-      util.setSuccess(200, 'Rate succesfully added');
+      util.setSuccess(200, res.__('Rate succesfully added'));
       return util.send(res);
     } catch (error) {
-      util.setError(400, error.message);
+      util.setError(
+        500,
+        res.__('There was an error while rating an accomodation'),
+      );
       return util.send(res);
     }
   };
@@ -23,36 +26,41 @@ class ReviewController {
       const reviewInformation = req.body;
       reviewInformation.userId = req.userId;
       const reviweAcc = await ReviewService.addReview(reviewInformation);
-      util.setSuccess(200, 'written review succesfully added');
+      util.setSuccess(200, res.__('written review succesfully added'));
       return util.send(res);
     } catch (error) {
-      util.setError(400, error.message);
+      util.setError(
+        500,
+        res.__('There was an error while reviewing accomodation'),
+      );
       return util.send(res);
     }
   };
   static getRating = async (req, res) => {
     try {
       const getRate = await RatingService.getRating();
-      if (getRate) {
-        util.setSuccess(200, 'Rating and Reviews', getRate);
-        return util.send(res);
+
+      if (!getRate) {
+        util.setError(404, res.__('This Rate not found'));
       }
-      util.setError(400, error.message);
+
+      util.setSuccess(200, res.__('Successfully fetched a rating', getRate));
+      return util.send(res);
     } catch (error) {
-      util.setError(404, error.message);
+      util.setError(500, res.__('There was an error while fetching Ratings'));
       return util.send(res);
     }
   };
   static getReview = async (req, res) => {
     try {
       const getReview = await ReviewService.getReview();
-      if (getReview) {
-        util.setSuccess(200, 'Reviews', getReview);
-        return util.send(res);
+      if (!getReview) {
+        util.setError(404, res.__('This Review not found'));
       }
-      util.setError(400, error.message);
+
+      util.setSuccess(200, res.__('Successfully fetched a review', getReview));
     } catch (error) {
-      util.setError(404, error.message);
+      util.setError(500, res.__('There was an error while fecting a review'));
       return util.send(res);
     }
   };
--- src/database/seeders/20210129093753-locations.js
+++ src/database/seeders/20210129093753-locations.js
@@ -1,11 +1,29 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Locations', [{
-      name: 'Kigali',
-      location_id: '10000',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }], {});
+    await queryInterface.bulkInsert(
+      'Locations',
+      [
+        {
+          name: 'Kigali',
+          location_id: '10000',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Musanze',
+          location_id: '20000',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Gisenyi',
+          location_id: '30000',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
